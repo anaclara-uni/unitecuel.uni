35c2958708733cdb2fe4df6ca3ef1e957049c056
anaclara-uni <272368@unifio.edu.br>
2025-08-21 23:40:22 +0000

    aula 21/08/2025

---
+++ public/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+</head>
+<body>
+    <h1> Página Inicial</h1>
+    <p> Primeiro Exemplo de Página Dinâmica</p>
+    <?php
+   // require ('./includes/naotem.php');//
+    include ('./includes/teste.php');
+    include_once ('./includes/teste.php');
+    
+    ?>
+</body>
+</html>
