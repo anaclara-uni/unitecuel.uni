ab17937ca50086701cfcf977cc5e1777744a15a0
anaclara-uni <272368@unifio.edu.br>
2025-09-04 01:43:32 +0000

    exercicio 1 03/09/2025

---
+++ public/exercicio1.php
@@ -0,0 +1,50 @@
+
+    <?php 
+    for($cont1 = 1;$cont1 <= 10;$cont ){
+echo "<li>" . ($cont1 ++ * 2) . "</li>";
+    }
+    ?> 
+    //exercicio resolvido// 
+    <?php
+    $numA = 1; 
+    $numB = 1; 
+      //exercicio resolvido vetores//
+    $sequencia = [1, 1]; 
+    for($cont = 2;$cont < 10; $cont++){
+   $sequencia[$cont] = $sequencia[$cont-1] + $sequencia[$cont-2];
+    }
+
+
+    //var_dump($sequencia);//
+    ?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+      
+        <title>Sequência de fibonacci</title>
+    </head>
+    <body>
+        <ul> 
+            <li><?= $numA ?></li>
+            <li><?= $numB ?></li>
+       
+            <?php for($cont = 0 ; $cont < 8 ; $cont ++): ?>
+          <?php 
+          $aux = $numA + $numB; 
+          $numA = $numB; 
+          $numB = $aux; 
+          ?>     
+          <li><?= $numB ?></li>  
+<?php endfor; ?>
+ </ul>
+ <h1>Usando vetor</h1>
+ <ul>
+    <?php foreach($sequencia as $num): ?>
+        <li><?= $num ?></li>
+        <?php endforeach ?> 
+ </ul>
+    </body>
+    </html>
+
+  
